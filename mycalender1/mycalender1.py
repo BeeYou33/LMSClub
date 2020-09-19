b6d61aebe9a2b559da1269fdb2b4147355d1e5db
BeeYou33 <71477022+BeeYou33@users.noreply.github.com>
2020-09-19 13:58:36 -0700

    Update mycalender1.py

--- mycalender1/mycalender1.py
+++ mycalender1/mycalender1.py
@@ -12,3 +12,17 @@ Tuesday 9:10-10:15         Math
 
 print(x)
     
+	
+#Result:
+
+#Day 	Time 		   Subject
+#Monday	9:10-10:15         Band
+    	#10:15-10:35        Break
+        #10:35-11:40        P.E
+        
+#Tuesday 9:10-10:15         Math
+       # 10:15-10:35        Break
+       # 10:35-11:40        Language Arts
+> 
+
+
