76b3a1ecd759aec2ab79b070255059d19adf82a8
BeeYou33 <71477022+BeeYou33@users.noreply.github.com>
2020-09-19 11:27:39 -0700

    Create mycalender1.py

---
+++ mycalender1/mycalender1.py
@@ -0,0 +1,14 @@
+#School scedhule 
+#done on Sep 19th 2020
+
+x = """ Day 	Time 		   Subject
+Monday	9:10-10:15         Band
+	10:15-10:35        Break
+        10:35-11:40        P.E
+        
+Tuesday 9:10-10:15         Math
+        10:15-10:35        Break
+        10:35-11:40        Language Arts"""
+
+print(x)
+    
