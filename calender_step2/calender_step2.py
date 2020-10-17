b9bcefe82f58b961eaaa8cde78c33d5c3e50742a
BeeYou33 <71477022+BeeYou33@users.noreply.github.com>
2020-10-17 14:15:22 -0700

    Create calender_step2.py

---
+++ calender_step2/calender_step2.py
@@ -0,0 +1,17 @@
+#function to print 
+def calendar():
+
+
+        x = """ Day 	           Time 		   Subject
+Monday            	9:10-10:15         Band
+	                 10:15-10:35        Break
+                     10:35-11:40        P.E
+        
+Tuesday               9:10-10:15         Math
+                      10:15-10:35        Break
+                       10:35-11:40        Language Arts"""
+        print(x)
+    
+
+#calling function to print my schedue
+calendar()
